9f8921646ff8b7f32c118e9743dde958445ad783
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-09-26 22:58:23 -0500

    feat: :construction: estructura inicial de módulo de persistencia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PersistenceModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
