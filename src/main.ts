e21ed9d95950450fe17ecbac81d0a2945852a3a3
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-09-28 18:39:48 -0500

    feat: :sparkles: implementa Swagger en el proyecto para mejorar la documentación de la API y facilitar su uso

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    transformOptions: { enableImplicitConversion: true },
-    forbidUnknownValues: true,
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      forbidUnknownValues: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('ANTARES API')
+    .setDescription('The API description for ANTARES')
+    .setVersion('1.0')
+    .addTag('security')
+    .addTag('projects-management')
+    .addTag('human-resources')
+    .addTag('knowledge-gaps')
+    .addTag('assessments')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(3000);
   console.log('Listening on port 3000 🚀');
 }
