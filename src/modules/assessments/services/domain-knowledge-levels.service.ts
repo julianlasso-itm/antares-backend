7ae26011503bf9a75c2a804cbe2f010f92703f39
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-10-14 02:27:09 -0500

    feat: :sparkles: se implementa búsqueda avanzada, filtrado y paginación en el método findAll de DomainKnowledgeLevelsService, optimizando consultas

--- src/modules/assessments/services/domain-knowledge-levels.service.ts
+++ src/modules/assessments/services/domain-knowledge-levels.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { BaseService } from '../../../common';
+import { Brackets, FindOptionsOrder } from 'typeorm';
+import { BaseService, Result } from '../../../common';
 import { DomainKnowledgeLevels } from '../../../common/modules/persistence/entities';
-import { DomainKnowledgeLevelsRepository } from '../../../common/modules/persistence/repositories';
+import {
+  DomainKnowledgeLevelsRepository,
+  FindAllResponse,
+} from '../../../common/modules/persistence/repositories';
 
 @Injectable()
 export class DomainKnowledgeLevelsService extends BaseService<
@@ -10,5 +14,106 @@ export class DomainKnowledgeLevelsService extends BaseService<
 > {
   constructor(protected readonly repository: DomainKnowledgeLevelsRepository) {
     super(repository);
+  }
+
+  async findAll(
+    page?: number,
+    size?: number,
+    order?: FindOptionsOrder<DomainKnowledgeLevels>,
+    searchField?: Array<keyof DomainKnowledgeLevels>,
+    searchTerm?: string,
+    filter?: string,
+  ): Promise<Result<FindAllResponse<DomainKnowledgeLevels>>> {
+    const repository = this.repository.repository;
+    const queryBuilder = repository.createQueryBuilder('domainKnowledgeLevels');
+
+    // Condición para excluir registros eliminados
+    queryBuilder.where('domainKnowledgeLevels.deletedAt IS NULL');
+
+    // Agregar condición de filtro si existe
+    if (filter) {
+      queryBuilder.andWhere(
+        'domainKnowledgeLevels.domainKnowledgeId = :filter',
+        {
+          filter,
+        },
+      );
+    }
+
+    // Agregar condiciones de búsqueda si existen
+    if (searchField && searchTerm) {
+      queryBuilder.andWhere(
+        new Brackets((qb) => {
+          searchField.forEach((field, index) => {
+            const condition = `(unaccent(domainKnowledgeLevels.${field as string}) ILIKE unaccent(:searchTerm) OR word_similarity(domainKnowledgeLevels.${field as string}, :searchTerm) > 0.2)`;
+
+            if (index === 0) {
+              qb.where(condition, { searchTerm: `%${searchTerm}%` });
+            } else {
+              qb.orWhere(condition, { searchTerm: `%${searchTerm}%` });
+            }
+          });
+        }),
+      );
+    }
+
+    // Ordenar resultados si se especifica
+    if (order) {
+      Object.entries(order).forEach(([key, value]) => {
+        queryBuilder.addOrderBy(
+          `domainKnowledgeLevels.${key}`,
+          value as 'ASC' | 'DESC',
+        );
+      });
+    }
+
+    // Paginación
+    if (page !== undefined && size !== undefined) {
+      queryBuilder.skip(page * size).take(size);
+    }
+
+    // Relaciones
+    queryBuilder
+      .leftJoinAndSelect(
+        'domainKnowledgeLevels.domainKnowledge',
+        'domainKnowledge',
+      )
+      .leftJoinAndSelect('domainKnowledge.technologyItem', 'technologyItem')
+      .leftJoinAndSelect('technologyItem.technologyType', 'technologyType')
+      .leftJoinAndSelect('domainKnowledgeLevels.level', 'level')
+      .leftJoinAndSelect(
+        'domainKnowledgeLevels.configurationLevel',
+        'configurationLevel',
+      );
+
+    // Selección de campos específicos
+    queryBuilder.select([
+      'domainKnowledgeLevels.domainKnowledgeId',
+      'domainKnowledgeLevels.domainKnowledgeLevelId',
+      'domainKnowledgeLevels.configurationLevelId',
+      'domainKnowledgeLevels.levelId',
+      'domainKnowledgeLevels.status',
+      'domainKnowledgeLevels.createdAt',
+      'domainKnowledgeLevels.updatedAt',
+      'domainKnowledgeLevels.deletedAt',
+      'domainKnowledge.domainKnowledgeId',
+      'domainKnowledge.domain', // Nombre del dominio
+      'technologyItem.technologyItemId',
+      'technologyItem.name', // Nombre de la tecnología
+      'technologyType.technologyTypeId',
+      'technologyType.name', // Nombre del tipo de tecnología
+      'level.levelId',
+      'level.name', // Nombre del nivel
+      'configurationLevel.configurationLevelId',
+      'configurationLevel.name', // Nombre de la configuración
+    ]);
+
+    // Ejecutar la consulta
+    const [result, total] = await queryBuilder.getManyAndCount();
+
+    return Result.ok({
+      data: result,
+      total,
+    });
   }
 }
